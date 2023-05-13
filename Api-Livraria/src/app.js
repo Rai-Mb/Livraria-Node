569d22b6249aa9a34ca4f1b06505a3280f3f6574
Rai <rmbraibarreira@gmail.com>
2023-05-13 06:37:41 -0300

    alteração na mensagem de conexão

--- Api-Livraria/src/app.js
+++ Api-Livraria/src/app.js
@@ -4,7 +4,7 @@ import routes from "./routes/Index.js"
 
 db.on('error', console.log.bind(console, 'Erro de conexão'))
 db.once('open', () => {
-    console.log('conexão com o banco feita com sucesso')
+    console.log('conexão com o banco realizado com sucesso')
 })
 
 const app = express()
